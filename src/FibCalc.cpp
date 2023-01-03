95c4ea6b2d7e4032d8d83b6331c32ef7f63c7217
maciub1 <92275332+maciub1@users.noreply.github.com>
2023-01-03 21:46:37 +0100

    punkt 1

---
+++ src/FibCalc.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+void credits()
+{
+    std::cout << "\nFibCalc\n";
+    std::cout << "Maciej Banaszak\n";
+    std::cout << "GR01\n";
+}
+
+int main()
+{
+    credits();
+
+    int w = 1;
+    while (w > 0)
+    {
+        int l1 = 0, l2 = 1, l3 = 0;
+
+        std::cout << "Podaj liczbe elementow ciagu Fibonacciego lub -1 aby wyjsc: ";
+        std::cin >> w;
+
+        if (w > 0)
+        {
+            for (int i = 1; i <= w; ++i)
+            {
+
+                if (i == 1)
+                {
+                    std::cout << l1 << ", ";
+                    continue;
+                }
+                if (i == 2)
+                {
+                    std::cout << l2 << ", ";
+                    continue;
+                }
+                l3 = l1 + l2;
+                l1 = l2;
+                l2 = l3;
+
+                std::cout << l3 << ", ";
+            }
+            credits();
+        }
+        else
+            break;
+    }
+    std::cout << "Wychodzenie z programu ...";
+    credits();
+
+    return 0;
+}
